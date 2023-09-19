54262cc10616a4d727ef62388a1cd7de9b8bacce
Yacine-df <ycn.diaf@gmail.com>
2023-09-19 23:34:04 +0200

    test

---
+++ resources/js/Pages/main.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<template>
+    <div class="grid place-items-center p-4 font-semibold">
+        Welcome, Welcome, Laravel with Vue and Inertia !
+    </div>
+</template>
+
+<style>
+
+</style>
